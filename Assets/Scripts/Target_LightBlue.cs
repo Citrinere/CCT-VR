6ca1bb36af40fbbeb0391bdaae1bd517b5bd63a4
TheFlow3107 <105515705+TheFlow3107@users.noreply.github.com>
2022-05-14 11:25:41 +0200

    Adding the Project to Github
    Hopping that this time everything will be in it.

---
+++ Assets/Scripts/Target_LightBlue.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target_LightBlue : MonoBehaviour
+{
+    public bool isDestroyed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isDestroyed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDestroyed == true)
+        {
+            Debug.Log("Light-Blue was Destroyed.");
+            ScoreManager.instance.AddPoints("Light-Blue");
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Balls")
+        {
+            isDestroyed = true;
+        }
+    }
+}
