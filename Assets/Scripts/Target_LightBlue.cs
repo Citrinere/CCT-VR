04f7f2c892d650a91e4efce76cac8146f7f83112
TheFlow3107 <105515705+TheFlow3107@users.noreply.github.com>
2022-05-24 15:27:58 +0200

    Update Target_LightBlue.cs
    Adding Diagonal Movement on the x & y axis

--- Assets/Scripts/Target_LightBlue.cs
+++ Assets/Scripts/Target_LightBlue.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Target_LightBlue : MonoBehaviour
 {
     public bool isDestroyed;
+    float xStart;
+    float xChange;
+    float yStart;
+    float yChange;
 
     public Vector3 playerPosition;
 
@@ -13,12 +17,20 @@ public class Target_LightBlue : MonoBehaviour
     {
         isDestroyed = false;
         playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+        xStart = transform.position.x;
+        xChange = Random.Range(1f, 2f) * Time.deltaTime;
+        yStart = transform.position.y;
+        yChange = Random.Range(1f, 2f) * Time.deltaTime;
     }
 
     // Update is called once per frame
     void Update()
     {
         TargetRotation();
+
+        //MoveUpDown();
+        //MoveLeftRight();
+        MoveDiagonal();
 
         if (isDestroyed == true)
         {
@@ -42,5 +54,43 @@ public class Target_LightBlue : MonoBehaviour
         Vector3 lookVector = playerPosition - transform.position;
 
         transform.rotation = Quaternion.LookRotation(lookVector);
+    }
+
+    //void MoveLeftRight()
+    //{
+    //    float xNew = transform.position.x + xChange;
+
+    //    transform.position = new Vector3(xNew, transform.position.y, transform.position.z);
+
+    //    if ((xNew > xStart + 3) || (xNew < xStart - 2))
+    //    {
+    //        xChange = -1 * xChange;
+    //    }
+    //}
+
+    //void MoveUpDown()
+    //{
+    //    float yNew = transform.position.y + yChange;
+
+    //    transform.position = new Vector3(transform.position.x, yNew, transform.position.z);
+
+    //    if ((yNew > yStart + 3) || (yNew < yStart - 2))
+    //    {
+    //        yChange = -1 * yChange;
+    //    }
+    //}
+
+    void MoveDiagonal()
+    {
+        float yNew = transform.position.y + yChange;
+        float xNew = transform.position.x + xChange;
+
+        transform.position = new Vector3(xNew, yNew, transform.position.z);
+
+        if (((xNew > xStart + 3) || (xNew < xStart - 2)) && ((yNew > yStart + 3) || (yNew < yStart - 2)))
+        {
+            yChange = -1 * yChange;
+            xChange = -1 * xChange;
+        }
     }
 }
